f30e43f66784ba236581620b640cf2cab3d0a560
Dodkoo23 <92848160+Dodkoo23@users.noreply.github.com>
2022-01-11 22:45:41 +0100

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+
+    float m, h, bmi;
+
+
+    cout << "Zadaj váhu v kilách : ";
+    cin >> m;
+    cout << "Zadaj výšku v metroch : ";
+    cin >> h;
+
+
+    bmi = m / (h * h);
+
+    if (bmi < 18, 5)
+        cout << "Podváha" << endl;
+    else if (bmi > 18, 5 && bmi < 24, 99)
+        cout << "Optimálna váha" << endl;
+    else if (bmi > 25 && bmi < 29,99)
+        cout << "Nadváha" << endl;
+    else if (bmi > 30)
+        cout << "Obezita" << endl;
+
+            
+            
+    system("pause>0");
+
+}
